34b09a493d5ff3e09884323585c98de5a600580d
Roberto Pacheco <robertopvc@gmail.com>
2023-04-21 10:09:28 -0300

    remove unnecessary files

--- run_extracting_inference_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 extracting_inference_data.py --n_branches 1 --model_name mobilenet --model_id 1
-python3 extracting_inference_data.py --n_branches 1 --model_name mobilenet --model_id 2
-python3 extracting_inference_data.py --n_branches 1 --model_name mobilenet --model_id 3
-python3 extracting_inference_data.py --n_branches 3 --model_name mobilenet --model_id 1
-python3 extracting_inference_data.py --n_branches 3 --model_name mobilenet --model_id 3
-python3 extracting_inference_data.py --n_branches 5 --model_name mobilenet --model_id 1
-python3 extracting_inference_data.py --n_branches 5 --model_name mobilenet --model_id 3
